ce17649ddbfa27aec965cf6bacfa1670e3a7a893
Georgi Kamacharov <gkamacharov@gmail.com>
2021-01-26 13:46:19 -0500

    add mapping unit tests

--- aiof.messaging.data/AutoMappingProfile.cs
+++ aiof.messaging.data/AutoMappingProfile.cs
@@ -19,14 +19,6 @@ namespace aiof.messaging.data
                 .ForMember(x => x.Cc, o => o.MapFrom(s => string.Join(",", s.Cc)))
                 .ForMember(x => x.Bcc, o => o.MapFrom(s => string.Join(",", s.Bcc)));
 
-            CreateMap<IEmailMessage, EmailMessageEntity>()
-                .ForMember(x => x.From, o => o.MapFrom(s => s.From))
-                .ForMember(x => x.To, o => o.MapFrom(s => s.To))
-                .ForMember(x => x.Subject, o => o.MapFrom(s => s.Subject))
-                .ForMember(x => x.Cc, o => o.MapFrom(s => s.Cc))
-                .ForMember(x => x.Bcc, o => o.MapFrom(s => s.Bcc))
-                .ForMember(x => x.Raw, o => o.MapFrom(s => JsonConvert.SerializeObject(s)));
-
             CreateMap<IMessage, MessageEntity>()
                 .ForMember(x => x.PublicKey, o => o.MapFrom(s => s.PublicKey))
                 .ForMember(x => x.Type, o => o.MapFrom(s => s.Type))
@@ -34,6 +26,14 @@ namespace aiof.messaging.data
                 .ForMember(x => x.Created, o => o.MapFrom(s => s.Created))
                 .ForMember(x => x.Raw, o => o.MapFrom(s => JsonConvert.SerializeObject(s)))
                 .ForAllOtherMembers(x => x.Ignore());
+
+            CreateMap<IEmailMessage, EmailMessageEntity>()
+                .ForMember(x => x.From, o => o.MapFrom(s => s.From))
+                .ForMember(x => x.To, o => o.MapFrom(s => s.To))
+                .ForMember(x => x.Subject, o => o.MapFrom(s => s.Subject))
+                .ForMember(x => x.Cc, o => o.MapFrom(s => s.Cc))
+                .ForMember(x => x.Bcc, o => o.MapFrom(s => s.Bcc))
+                .ForMember(x => x.Raw, o => o.MapFrom(s => JsonConvert.SerializeObject(s)));
         }
     }
 }
